602d3bebbea7c60c8fce06a6e240f3f949772825
dilan <125643732+therealdilan@users.noreply.github.com>
2023-11-16 15:56:10 +0400

    working w/ slices

--- slice.go
+++ slice.go
@@ -1,5 +1,35 @@
-package main 
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+var close = true
 
 func main() {
-	
+
+	var sli = make([]int, 3)
+	var input string
+
+	for close == true {
+		fmt.Println("Enter an Integer, type X to exit: ")
+		fmt.Scanln(&input)
+
+		if input == "X" || input == "x" {
+			close = false
+			break
+		}
+
+		val, err := strconv.Atoi(input)
+
+		if err != nil {
+			fmt.Println("Invalid Input!")
+		} else {
+			sli = append(sli, val)
+			fmt.Println(sli)
+		}
+
+	}
+
 }
